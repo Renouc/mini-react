7b025b38cbf97786b42fd918f4f3b2f33281632a
Renouc <liuchuan@shoplazza.com>
2025-05-20 23:20:58 +0800

    循环遍历

--- packages/reconciler/WorkLoop.ts
+++ packages/reconciler/WorkLoop.ts
@@ -4,16 +4,43 @@ import { beginWork } from "./BeginWork";
 import { completeWork } from "./CompleteWork";
 import { Fiber } from "./ReactInternalTypes";
 
-export function workLoop(fiber: Fiber) {
-  const child = beginWork(fiber);
-  if (child) {
-    workLoop(child);
-  }
+// 正在处理的节点
+let workInProgress: Fiber | null = null;
 
-  completeWork(fiber);
-  if (fiber.sibling) {
-    workLoop(fiber.sibling);
+// 完成单元工作，对节点进行回溯遍历，并触发完成工作
+
+function completeUnitOfWork(fiber: Fiber | null) {
+  // 已经完成的节点
+  let completedFiber: Fiber | null = fiber;
+
+  do {
+    completeWork(completedFiber!);
+    const siblingFiber = completedFiber!.sibling;
+    if (siblingFiber) {
+      workInProgress = siblingFiber;
+      return;
+    }
+
+    completedFiber = completedFiber!.return;
+    workInProgress = completedFiber;
+  } while (completedFiber);
+}
+
+function performUnitOfWork(fiber: Fiber) {
+  let next: Fiber | null = beginWork(fiber);
+
+  if (next) {
+    workInProgress = next;
   } else {
-    return;
+    // 回溯
+    completeUnitOfWork(fiber);
+  }
+}
+
+// 循环遍历
+export function workLoop(container_fiber: Fiber) {
+  workInProgress = container_fiber;
+  while (workInProgress) {
+    performUnitOfWork(workInProgress);
   }
 }
